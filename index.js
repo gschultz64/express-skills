57bfeebb12fbbe32e79d9a838ea01303604798fb
Genevieve Schultz <genschultz@gmail.com>
2018-06-13 09:16:20 -0700

    deleted note comments

--- index.js
+++ index.js
@@ -9,23 +9,20 @@ app.use(bp.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', function (req, res) {
-  // TODO: Add links to index.html so users can navigate
   res.sendFile(__dirname + '/static/html/index.html');
 });
 
 app.get('/skills', function (req, res) {
   var skills = fs.readFileSync('./data.json');
   skills = JSON.parse(skills);
-  // TODO: Render this array of skills into an EJS template
   res.render('skills', {skills: skills});
 });
 
-// TODO: Add GET route that returns static page containing a form
 app.get('/skills/new', function (req, res) {
   res.sendFile(__dirname + '/static/html/new.html');
 });
 
-// TODO: Add POST route that writes new skill to the file, redirects to skills index
+
 app.post('/skills', function (req, res) {
   // read the data file
   var skills = fs.readFileSync('./data.json');
@@ -38,6 +35,5 @@ app.post('/skills', function (req, res) {
   // redirect to skills list page 
   res.redirect('/skills');
 });
-// TODO: Form should have action='/skills' and method='POST'
 
 app.listen(process.env.PORT || 3000);
